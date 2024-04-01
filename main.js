fa405eb5754545de968115608dfd54e82dd34ecb
Aqsa khan <161198603+Aqlish@users.noreply.github.com>
2024-04-01 13:09:38 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+var email = "aqlish@gmail.com";
+var password = "1234";
+var code = "000";
+if (email == "aqsakhan@gmail.com" && password == "12345") {
+    console.log("kindly provide code");
+}
+if (code == "000") {
+    console.log("correct code");
+}
+else {
+    console.log("incorrect code");
+}
+if (email == "aqlish@gmail.com" && password == "1234") {
+    console.log("kindly provide code");
+}
+if (code == "000") {
+    console.log("log in");
+}
+else {
+    console.log("incorrect code");
+}
---
+++ main.ts
@@ -0,0 +1,22 @@
+let email = "aqlish@gmail.com"
+let password = "1234"
+let code = "000"
+
+if(email == "aqsakhan@gmail.com" && password == "12345"){
+    console.log("kindly provide code")
+}
+if(code == "000"){
+    console.log("correct code")
+}
+else{
+    console.log("incorrect code")
+}
+else if(email == "aqlish@gmail.com" && password == "1234"){
+    console.log("kindly provide code")
+}
+if(code == "000"){
+    console.log("log in")
+}
+else{
+    console.log("incorrect code")
+}
